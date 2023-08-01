14b857bf6ab951584fe4c0f90932859e14f801c5
pncoelho <pncoelho@outlook.pt>
2023-08-01 12:14:43 +0100

    feat: Make provision script print package version

--- shell_provision_script.sh
+++ shell_provision_script.sh
@@ -1,35 +1,22 @@
 #! /bin/bash
 
-# Update packages and sources
-sudo apt-get update
+# Variables - System Info
+DISTRO_SHORT_CODENAME=$(lsb_release -cs)
+PACKAGE_ARCHITECTURE=$(dpkg --print-architecture)
 
-# Install packages needed for installing Azure CLI
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+# Variables - Package Versions
+VERS_AZURE_CLI="2.50.\*"
+VERS_AZURE_FUNC_TOOLS="4.0.\*"
 
-# TODO: Refactor the installation of the Microsoft signing key so it only happens once
-
-# Download and install the Microsoft signing key needed for installing Azure CLI
-sudo mkdir -p /etc/apt/keyrings
-curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
-    gpg --dearmor |
-    sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
-sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
-
-# Add the Azure CLI software repository
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
-
-# Install the Microsoft package repository GPG key, to validate package integrity, needed for installing Azure Functions Core Tools
+# Install the Microsoft package repository GPG key, to validate package integrity, needed for installing Azure CLI and Azure Functions Core Tools
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 
-# Set up the APT source list needed for installing Azure Functions Core Tools
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
+# Set up the APT source list needed for installing Azure CLI
+sudo sh -c 'echo "deb [arch='$PACKAGE_ARCHITECTURE'] https://packages.microsoft.com/repos/azure-cli/ '$DISTRO_SHORT_CODENAME' main" > /etc/apt/sources.list.d/azure-cli.list'
 
-# Check the /etc/apt/sources.list.d/dotnetdev.list file for one of the appropriate Linux version strings
-# Ubuntu 20.04 -> focal
-grep 'focal' /etc/apt/sources.list.d/dotnetdev.list | wc -l
+# Set up the APT source list needed for installing Azure Functions Core Tools
+sudo sh -c 'echo "deb [arch='$PACKAGE_ARCHITECTURE'] https://packages.microsoft.com/repos/microsoft-ubuntu-'$DISTRO_SHORT_CODENAME'-prod '$DISTRO_SHORT_CODENAME' main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 # Update packages
 sudo apt-get update
@@ -39,9 +26,14 @@ sudo apt-get update
 sudo apt-get install -y dotnet-sdk-7.0
 
 # Installing a specific `azure-cli` version:
-# To view available versions with command:
-#   apt-cache policy azure-cli
-sudo apt-get install azure-cli=2.50.\*-1~focal
+# sudo apt-get install -y azure-cli=2.50.\*-1~focal
+sudo sh -c 'sudo apt-get install -y azure-cli='$VERS_AZURE_CLI''
 
 # Install the Core Tools package:
-sudo apt-get install azure-functions-core-tools-4
+# sudo apt-get install -y azure-functions-core-tools-4
+sudo sh -c 'sudo apt-get install -y azure-functions-core-tools-4='$VERS_AZURE_FUNC_TOOLS''
+
+# Getting installed versions of packages
+dotnet --info
+az –version
+func –version
